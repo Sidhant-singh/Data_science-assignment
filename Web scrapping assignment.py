0c8f45732cfd102a3d278bce873f724c7b5ac63a
Sidhant-singh <123365953+Sidhant-singh@users.noreply.github.com>
2024-01-25 00:05:20 +0530

    Add files via upload

---
+++ Web scrapping assignment.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+'''
+Q1. What is Web Scraping? Why is it Used? Give three areas where Web Scraping is used to get data.
+Web scraping is automated extraction of data from websites. 
+It's used for efficiency in collecting information. 
+Three areas include business research, price monitoring, and content aggregation.
+
+Q2. What are the different methods used for Web Scraping?
+Methods include manual scraping, regular expressions, 
+DOM parsing, web scraping libraries (e.g., Beautiful Soup), 
+and headless browsing with tools like Selenium.
+
+
+Q3. What is Beautiful Soup? Why is it used?
+Beautiful Soup is a Python library for parsing HTML and XML. 
+It simplifies navigation of HTML structures, making it easier to extract data during web scraping.
+
+Q4. Why is Flask used in this Web Scraping project?
+Flask is a lightweight web framework for Python. 
+It can be used to build a web interface, present scraped data, create API endpoints, and integrate with other Python libraries.
+
+
+Q5. Write the names of AWS services used in this project. Also, explain the use of each service.
+Amazon EC2: Hosts web scraping scripts.
+Amazon S3: Stores and manages scraped data.
+AWS Lambda: Executes periodic web scraping tasks.
+Amazon RDS: Stores structured data in a relational database.
+Amazon API Gateway: Creates, publishes, and manages APIs.
+Amazon CloudWatch: Monitors and logs the performance of the web scraping application.
+'''
+
+
+# In[ ]:
+
+
+
+
